802d1368b4b34d15b64cf2579fe0a88389c31683
Onlyartist9 <gyamfisamuel10@gmail.com>
2025-01-02 17:21:06 +0100

    Prototype interactive chat

--- augini/__init__.py
+++ augini/__init__.py
@@ -1,4 +1,5 @@
 from .core import Augini
 from .exceptions import APIError, DataProcessingError
+from .core import AuginiInteractiveChat
 
-__all__ = ['Augini', 'APIError', 'DataProcessingError']
+__all__ = ['Augini', 'APIError', 'DataProcessingError', 'AuginiInteractiveChat']
